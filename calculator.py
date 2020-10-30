727c71d84e023af24cf065c5cb165c9fc986e195
alekhya-hash <72356896+alekhya-hash@users.noreply.github.com>
2020-10-30 08:28:50 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -23,7 +23,7 @@ def button_add():
     math = "addition"
     f_num = int(first_number)
     e.delete(0, END)
-
+	
 
 def button_equal():
     second_number = e.get()
@@ -40,6 +40,12 @@ def button_equal():
 
     if math == "division":
         e.insert(0, f_num / int(second_number))
+	
+    if math == "percentile":
+        e.insert(0, f_num % int(second_number))
+	
+    if math == "power":
+        e.insert(0, f_num ** int(second_number))
 
 
 def button_subtraction():
@@ -49,7 +55,22 @@ def button_subtraction():
     math = "subtraction"
     f_num = int(first_number)
     e.delete(0, END)
-
+	
+def button_percentile():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "percentile"
+    f_num = int(first_number)
+    e.delete(0, END)
+	
+def button_power():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "power"
+    f_num = int(first_number)
+    e.delete(0, END)	
 
 def button_multiply():
     first_number = e.get()
@@ -79,34 +100,41 @@ button_7= Button(root,text="7",padx=40,pady=20,command=lambda: button_click(7))
 button_8= Button(root,text="8",padx=40,pady=20,command=lambda: button_click(8))
 button_9= Button(root,text="9",padx=40,pady=20,command=lambda: button_click(9))
 button_0= Button(root,text="0",padx=40,pady=20,command=lambda: button_click(0))
-button_add=Button(root,text="+",padx=39,pady=20,command= button_add)
-button_equal=Button(root,text="=",padx=90,pady=20,command= button_equal)
-button_clear=Button(root,text="clear",padx=71,pady=20,command= button_clear)
+button_add=Button(root,text="+",padx=40,pady=20,command= button_add)
+button_equal=Button(root,text="=",padx=40,pady=20,command= button_equal)
+button_clear=Button(root,text="clear",padx=50,pady=20,command= button_clear)
 
 button_subtract=Button(root,text="-",padx=39,pady=20,command= button_subtraction)
 button_multiply=Button(root,text="*",padx=40,pady=20,command= button_multiply)
 button_divide=Button(root,text="/",padx=40,pady=20,command= button_divide)
 
+button_percentile=Button(root,text="%",padx=40,pady=20,command= button_percentile)
+button_power=Button(root,text="/",padx=40,pady=20,command= button_power)
+#button_divide=Button(root,text="/",padx=40,pady=20,command= button_divide)
+
+
 button_1.grid(row=3,column=0)
 button_2.grid(row=3,column=1)
 button_3.grid(row=3,column=2)
 
-button_4.grid(row=2,column=0)
-button_5.grid(row=2,column=1)
-button_6.grid(row=2,column=2)
+button_4.grid(row=3,column=3)
+button_5.grid(row=2,column=0)
+button_6.grid(row=2,column=1)
 
-button_7.grid(row=1,column=0)
-button_8.grid(row=1,column=1)
-button_9.grid(row=1,column=2)
+button_7.grid(row=2,column=2)
+button_8.grid(row=2,column=3)
+button_9.grid(row=1,column=0)
 
-button_0.grid(row=4,column=0)
-button_clear.grid(row=4,column=1,columnspan=2)
-button_add.grid(row=5,column=0)
-button_equal.grid(row=5,column=1,columnspan=2)
+button_0.grid(row=1,column=1)
+button_clear.grid(row=6,column=0,columnspan=2)
+button_add.grid(row=1,column=2)
+button_equal.grid(row=1,column=3,columnspan=2)
 
-button_subtract.grid(row=6,column=0)
-button_multiply.grid(row=6,column=1)
-button_divide.grid(row=6,column=2)
+button_subtract.grid(row=5,column=0)
+button_multiply.grid(row=5,column=1)
+button_divide.grid(row=5,column=2)
 
+button_percentile.grid(row=5,column=3)
+button_power.grid(row=6,column=1)
 
 root.mainloop()
